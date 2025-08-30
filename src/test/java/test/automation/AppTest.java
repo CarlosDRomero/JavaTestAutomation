2ea7b466912fe4e4891123d32b2568c8e33f5fd1
CarlosDRomero <cdavid.romeror@gmail.com>
2025-08-30 17:26:42 -0500

    testing login with selenium methods

--- src/test/java/test/automation/AppTest.java
+++ src/test/java/test/automation/AppTest.java
@@ -1,19 +1,39 @@
 package test.automation;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class AppTest {
 
     /**
      * Rigorous Test :-)
      */
+    WebDriver driver;
+    String URL;
+
+    @BeforeMethod
+    public void before(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+    }
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+        WebElement nameInput = driver.findElement(By.name("user-name"));
+        nameInput.click();
+        nameInput.sendKeys("standard_user");
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        passwordInput.sendKeys("secret_sauce");
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
     }
 }
