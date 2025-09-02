c98f7cf4cee7c83dcfd5a379db67b6a8676f657b
CarlosDRomero <cdavid.romeror@gmail.com>
2025-09-02 13:58:20 -0500

    Disable 'Change your password' popup

--- src/test/java/test/automation/tests/LoginSteps.java
+++ src/test/java/test/automation/tests/LoginSteps.java
@@ -3,10 +3,17 @@ package test.automation.tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import test.automation.pages.LoginPage;
+import test.automation.pages.ShopPage;
+import test.automation.pages.ShoppingCartPage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginSteps {
     WebDriver driver;
@@ -14,16 +21,29 @@ public class LoginSteps {
     @BeforeMethod
     public void beforeMethod() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // Con estas líneas puedo desactivar un popup que me salía en chrome de que encontraba la contraseña del login como una contraseña insegura
+        // Esta solución es una adaptación de lo encontrado en este vídeo con C#: https://www.youtube.com/watch?v=eNsC5dbjPjw
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+        options.setExperimentalOption("prefs", prefs);
+
+
+        driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
         loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
     }
     @Parameters({"userName", "password"})
     @Test
     public void login(String userName, String password) {
         loginPage.enterUserName(userName);
         loginPage.enterPassword(password);
-        loginPage.clickLoginButton();
+        ShopPage shopPage = loginPage.clickLoginButton();
+        shopPage.addRandomProduct();
     }
+
 }
